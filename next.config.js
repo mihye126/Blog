9a9da4ce44f4a5765897aaa5d878768822ca1d0b
mihye126 <mihea12@gmail.com>
2023-03-05 22:30:37 +0900

    next.config to loader

--- next.config.js
+++ next.config.js
@@ -1,28 +1,11 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true'
-})
-const withPlugins = require('next-compose-plugins');
+// const withBundleAnalyzer = require('@next/bundle-analyzer')({
+//   enabled: process.env.ANALYZE === 'true'
+// })
 
-module.exports = withPlugins([
-  [withBundleAnalyzer({
-    staticPageGenerationTimeout: 300,
-    images: {
-      domains: [
-        'www.notion.so',
-        'notion.so',
-        'images.unsplash.com',
-        'pbs.twimg.com',
-        'abs.twimg.com',
-        's3.us-west-2.amazonaws.com',
-        'ibb.co',
-        'static.solved.ac'
-      ],
-      formats: ['image/avif', 'image/webp'],
-      dangerouslyAllowSVG: true,
-      contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;"
-    }
-  })], 
-  // your other plugins here
-])
-
+moduel.exports = {
+  images: {
+      loader: 'akamai',
+      path: '/',
+  }
+}
