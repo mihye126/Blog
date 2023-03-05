36e3593916d2a5a6cdcc9e79ceef9f830b5f34f9
mihye126 <mihea12@gmail.com>
2023-03-05 23:03:35 +0900

    Fix next.config

--- next.config.js
+++ next.config.js
@@ -2,23 +2,31 @@
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true'
 })
+const withPlugins = require("next-compose-plugins");
+const optimizedImages = require("next-optimized-images");
 
-module.exports = withBundleAnalyzer({
-  staticPageGenerationTimeout: 300,
-  images: {
-    domains: [
-      'www.notion.so',
-      'notion.so',
-      'images.unsplash.com',
-      'pbs.twimg.com',
-      'abs.twimg.com',
-      's3.us-west-2.amazonaws.com',
-      'static.solved.ac',
-      'ibb.co',
-      ''
-    ],
-    formats: ['image/avif', 'image/webp'],
-    dangerouslyAllowSVG: true,
-    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;"
-  }
-})
+const nextConfig = {};
+
+module.exports = withPlugins([
+  [optimizedImages, {
+    optimizeImages: false,
+  }],[withBundleAnalyzer({
+    staticPageGenerationTimeout: 300,
+    images: {
+      domains: [
+        'www.notion.so',
+        'notion.so',
+        'images.unsplash.com',
+        'pbs.twimg.com',
+        'abs.twimg.com',
+        's3.us-west-2.amazonaws.com',
+        'static.solved.ac',
+        'ibb.co',
+        ''
+      ],
+      formats: ['image/avif', 'image/webp'],
+      dangerouslyAllowSVG: true,
+      contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;"
+    }
+  })]
+], nextConfig);
