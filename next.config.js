f851a6797725e1433c331ceb6e03fbdc57274868
mihye126 <mihea12@gmail.com>
2023-03-05 22:36:54 +0900

    next.config to loader

--- next.config.js
+++ next.config.js
@@ -2,10 +2,18 @@
 // const withBundleAnalyzer = require('@next/bundle-analyzer')({
 //   enabled: process.env.ANALYZE === 'true'
 // })
-
-moduel.exports = {
+module.exports = {
   images: {
-      loader: 'akamai',
-      path: '/',
-  }
-}
+    loader: 'imgix',
+    path: [
+      'www.notion.so',
+      'notion.so',
+      'images.unsplash.com',
+      'pbs.twimg.com',
+      'abs.twimg.com',
+      's3.us-west-2.amazonaws.com',
+      'ibb.co/',
+      'static.solved.ac'
+    ],
+  },
+};
