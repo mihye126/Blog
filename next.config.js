0b25519b7383ddbd5698e71a4c36f36c09698141
mihye126 <mihea12@gmail.com>
2023-03-05 22:54:40 +0900

    fix error

--- next.config.js
+++ next.config.js
@@ -1,19 +1,24 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-// const withBundleAnalyzer = require('@next/bundle-analyzer')({
-//   enabled: process.env.ANALYZE === 'true'
-// })
-module.exports = {
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true'
+})
+
+module.exports = withBundleAnalyzer({
+  staticPageGenerationTimeout: 300,
   images: {
-    loader: 'imgix',
-    path: [
+    domains: [
       'www.notion.so',
       'notion.so',
       'images.unsplash.com',
       'pbs.twimg.com',
       'abs.twimg.com',
       's3.us-west-2.amazonaws.com',
-      'ibb.co/',
-      'static.solved.ac'
+      'static.solved.ac',
+      'ibb.co',
+      ''
     ],
-  },
-};
+    formats: ['image/avif', 'image/webp'],
+    dangerouslyAllowSVG: true,
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;"
+  }
+})
