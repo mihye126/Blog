0400445118b7917b27f85e7c35cdafcd8411574b
mihea12 <mihea12@gmail.com>
2023-03-09 16:21:40 +0900

    [Fix] useRef error

--- components/Utterances.tsx
+++ components/Utterances.tsx
@@ -1,4 +1,4 @@
-import React, { createRef, useLayoutEffect, useState, useEffect} from 'react';
+import React, { createRef, useLayoutEffect} from 'react';
 import {utterancesRepo} from '../lib/config'
 
 const src = 'https://utteranc.es/client.js';
@@ -7,8 +7,27 @@ export const Utterances: React.FC<{
   isBlogPost: boolean
 }> = ({ isBlogPost }) => {
 
-  
-  const [repo, setRepo] = useState(utterancesRepo);
+  const containerRef = createRef<HTMLDivElement>();
+
+  useLayoutEffect(() => {
+    const utterances = document.createElement('script');
+
+    const attributes = {
+        src,
+        'repo':utterancesRepo,
+        'issue-term': 'pathname',
+        label: 'comment',
+        theme: 'github-light',
+        crossOrigin: 'anonymous',
+        async: 'true',
+    };
+
+    Object.entries(attributes).forEach(([key, value]) => {
+        utterances.setAttribute(key, value);
+    });
+
+    containerRef.current.appendChild(utterances);
+}, [utterancesRepo]);
 
   if (! isBlogPost || utterancesRepo==null) {
     return null
@@ -16,30 +35,8 @@ export const Utterances: React.FC<{
 
   // only display comments and page actions on blog post pages
   if (isBlogPost && utterancesRepo!=null) {
-    if (typeof document === 'undefined') {
-      React.useLayoutEffect = React.useEffect;
-    }
-    const containerRef = createRef<HTMLDivElement>();
-
-    useLayoutEffect(() => {
-      const utterances = document.createElement('script');
-
-      const attributes = {
-          src,
-          'repo':utterancesRepo,
-          'issue-term': 'pathname',
-          label: 'comment',
-          theme: 'github-light',
-          crossOrigin: 'anonymous',
-          async: 'true',
-      };
-
-      Object.entries(attributes).forEach(([key, value]) => {
-          utterances.setAttribute(key, value);
-      });
-
-      containerRef.current.appendChild(utterances);
-  }, [repo]);
+    
+ 
 
   return <div className='notion-hr' ><div ref={containerRef} className="utterances"/></div>;
   }
