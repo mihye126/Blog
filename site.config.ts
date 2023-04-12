4588632af3efa4a4d5372f981234be25e89911b2
mihea12 <mihea12@gmail.com>
2023-04-12 16:54:24 +0900

    [Fix] redis disabled

--- site.config.ts
+++ site.config.ts
@@ -36,7 +36,7 @@ export default siteConfig({
   // whether or not redis is enabled for caching generated preview images (optional)
   // NOTE: if you enable redis, you need to set the `REDIS_HOST` and `REDIS_PASSWORD`
   // environment variables. see the readme for more info
-  isRedisEnabled: true,
+  isRedisEnabled: false,
 
   //Utterances
   isUtterancesEnabled: true,
